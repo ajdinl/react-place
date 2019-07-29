2437078a203161e2a476a1d370ad707397182f6f
ajdinl <ajdinl>
2019-07-29 11:09:12 +0200

    trnin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyInfo from './components/MyInfo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyInfo />, document.getElementById('root'))
